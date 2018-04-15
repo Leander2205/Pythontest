f5c299f8448d96f2364b24689a2f0f12d6e30764
Leander2205 <s8lerudo@stud.uni-saarland.de>
2018-04-15 21:14:58 +0200

    new commitG:/Anaconda/python.exe

--- Programiieraufgabe.py
+++ Programiieraufgabe.py
@@ -1,21 +1,36 @@
-class Menschen:
-    Name =""
-    Nachname =""
-    Alter = 0
-def __init__ (self,Name,Nachname,Alter):
-   self.Name = Name
-   self.Nachname = Nachname
-   self.Alter = Alter
-def print_daten(self):
-    print ("Mensch:",self.name,"",self.nachname,"Alter",self.alter)
+class Mensch:
+    name = ""
+    nachname = ""
+    alter = 0
 
-class Student (Mensch):
-    mnr= 0
+    def __init__(self, name, nachname, alter):
+        self.name = name
+        self.nachname = nachname
+        self.alter = alter
+
+    def print_daten(self):
+        print('Mensch: ', self.name, " ", self.nachname, " Alter: ", self.alter)
+
+class Student(Mensch):
+    mnr = 0
     semester = 0
-    studiengang = ""    
-    
-    def __init__(self,mnr,semester,studiengang,Name,Nachname,Alter)
-        super (Student, self).__init__(Name,Nachname,Alter)
+    studiengang = ""
+
+    def __init__(self, mnr, semester, studiengang, name, nachname, alter):
+        super(Student, self).__init__(name, nachname, alter)
         self.mnr = mnr
         self.semester = semester
-        self.studiengang= studiengang
+        self.studiengang = studiengang
+
+    def print_daten(self):
+        super(Student, self).print_daten()
+        print('Mnr: ', self.mnr, ' Semester: ', self.semester, ' Studiengang: ', self.studiengang)
+Leander = Student(256654,4,"BachelorScience","Leander","S",19)
+Leander.print_daten()
+
+peter = Mensch("Peter","H",54)
+print("Ist Leander ein Student?")
+if isinstance(Leander,Student):
+    print("Ja")
+else:
+    print ("Nein")
