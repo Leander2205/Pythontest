2bb9e75172965557fd75441cad19c40ae46dd2be
Leander2205 <s8lerudo@stud.uni-saarland.de>
2018-04-15 15:59:23 +0200

    new

---
+++ Zipcrack.py
@@ -0,0 +1,20 @@
+#Zip File Brutforce
+import zipfile
+import itertools
+import string
+from threading import Thread
+
+def crack (zip, pwd):
+    try:
+        zip.extractall (pwd=str.encode(pwd))
+        print ("Success:"+pwd)
+    except:
+        pass
+
+zipFile= zipfile.ZipFile("")
+myLetters= string.ascii_letters + string.digits + string.punctuation
+for i in range (3,10):
+    for j in map ("".join, itertools.product(myLetters, repeat=i)):
+        t= Thread(target=crack,args=(zipFile,j))
+        t.start()
+
